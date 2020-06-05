931f0592c3a109452fe4069b8e9bd577511a9fa2
dlopes7 <davidribeirolopes@gmail.com>
2020-06-05 16:33:53 -0300

    Version 1.0.6
    State

--- src/custom_extension_davidlopes.py
+++ src/custom_extension_davidlopes.py
@@ -16,19 +16,28 @@ class CustomExtensionDavidLopes(RemoteBasePlugin):
         for navio in navios:
 
             log.info(navio["ship_name"])
+
+            # Cria o custom device
             id_navio = f'{meu_nome} {navio["ship_id"]}'
             nome_navio = f'{meu_nome} {navio["ship_name"]}'
-
             device = grupo.create_device(id_navio, nome_navio)
+
+            # Manda uma métrica absoluta (simples)
             device.absolute("combustivel", navio["fuel"])
 
+            # Métrica com dimensões
             for motor in navio["thrust"]:
                 device.absolute("potencia", motor["power"], dimensions={"Motor": motor["engine"]})
 
+            # Propriedades
             device.report_property("Tipo", navio.get("ship_type", "Desconhecido"))
             device.report_property("Porto", navio.get("home_port", "Desconhecido"))
 
+            # Topologia
             device.add_endpoint(navio["ship_ip"])
+
+            # Statetimeseries
+            device.state_metric("clima", navio["weather"])
 
     def get_ships(self):
         return requests.get(f"{self.config['url']}/v3/ships").json()
